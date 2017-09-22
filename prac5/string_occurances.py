b3fa16f0cc81fb4382380940f2fcc8bd317a49a7
megangodwin <megan.godwin@my.jcu.edu.au>
2017-09-22 12:15:38 +1000

    fixes basic function
    string_occurances.py from prac 5

--- prac5/string_occurances.py
+++ prac5/string_occurances.py
@@ -1,6 +1,19 @@
 """
 CP1404/CP5632 Practical
-Counts the occurances of words in a string
+Counts the occurrences of words in a string
 """
+
+string_to_evaluate = input("Text :")
+
+string_dict = {}
+string_dict = string_to_evaluate.split()
+
+for item in string_dict:
+    item in
+
+
+print(string_list)
+
+
 
 
